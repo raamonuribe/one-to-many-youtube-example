37a25727cdfcd4b62b62b4f115314e7727b81a20
Ramon <ramon@ramonuribe.com>
2021-03-02 08:06:29 -0700

    initialized project

---
+++ src/test/java/com/ramonuribe/onetomanywebservice/OneToManyWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.onetomanywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
